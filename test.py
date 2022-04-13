65829c8036ed9b3299ed918f05fb65ba42385f12
KIMHOONHEE <nmdkims@gmail.com>
2022-04-13 23:34:04 +0900

    추가 - 220413_TIL

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import sys
 
-N, M = map(int, sys.stdin.readline().split())
-matrixA = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+input =sys.stdin.readline
 
-M, K = map(int, sys.stdin.readline().split())
-matrixB = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+N = int(input())
 
-for n in range(N):
-    for k in range(K):
-        tmp = 0
-        for m in range(M):
-            tmp += matrixA[n][m] * matrixB[m][k]
+num =[0]*10001
 
-        sys.stdout.write(str(tmp) + " ")
-    sys.stdout.write("\n")
+for _ in range(N):
+    temp = int(input())
+    num[temp] += 1
+
+for i in range(10001):
+    if num[i] !=0:
+        for j in range(num[i]):
+            print(i)
